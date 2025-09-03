606b8b28e2a1819ecb83f14d39a0100f39927682
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-09-03 06:11:14 +0000

    full

--- ProductApp/RootFunction/__init__.py
+++ ProductApp/RootFunction/__init__.py
@@ -3,6 +3,6 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Redirect root URL to FrontendFunction
     return func.HttpResponse(
-        status_code=301,
+        status_code=302,  # temporary redirect
         headers={"Location": "/api/FrontendFunction"}
     )
